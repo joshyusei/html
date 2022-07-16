1ec4318308014865fc23e2f52ce683f214926d34
joshyusei <89175617+joshyusei@users.noreply.github.com>
2022-07-16 12:43:01 +0800

    Update home.js

--- home.js
+++ home.js
@@ -1,11 +1,13 @@
 function restart() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            alert("重新啟動中...")
-        }
-        else {
-            alert("重新啟動失敗")
+        if (this.readyState == 4 ) {
+            if ( this.status == 200) {
+                alert("重新啟動中...")
+            }
+            else {
+                alert("重新啟動失敗")
+            }
         }
     }
     xhttp.open("GET", "control?restart", true);
